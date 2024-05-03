e97d3fa7ec2aedb00018f62ebcbfd9c2ad6c580b
Patrick Ford <patrickfordp@gmail.com>
2024-05-02 21:53:37 -0700

    Start adding attendance

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,11 +1,9 @@
 using Application.Activities;
 using Domain;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
@@ -31,6 +29,12 @@ namespace API.Controllers
         {
             activity.Id = id;
             return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
         }
 
         [HttpDelete("{id}")]
